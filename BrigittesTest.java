42b499c8591ec7ecc2b12eafec7755cc6c4d3288
Brauch25 <brauch23@hnmcs.ca>
2023-02-03 08:58:15 -0500

    Hello from Brigitte

---
+++ BrigittesTest.java
@@ -0,0 +1,14 @@
+
+    /**
+ * Brigitte: GitHub sharing test 
+ * 
+ */
+public class BrigittesTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Grade 11s");
+        System.out.println("This is a test from Brigitte"); // 02/3/23 8:50am
+        System.out.println("Collaborate below!!");
+    }
+}
+
