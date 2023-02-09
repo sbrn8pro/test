ecbb97c5c62c49515acad09f19bc400c2ae09c63
agoddard-24 <124809251+agoddard-24@users.noreply.github.com>
2023-02-09 11:28:11 -0500

    Update BrigittesTest.java

--- BrigittesTest.java
+++ BrigittesTest.java
@@ -9,7 +9,7 @@ public class BrigittesTest {
         System.out.println("Hello Grade 11s");
         System.out.println("This is a test from Brigitte"); // 02/3/23 8:50am
         System.out.println("Collaborate below!!");
-        System.out.println("Once upon a time there was a frog. This frog wanted nothing more in life then to learn how to code.")
+        System.out.println("Once upon a time there was a frog. This frog wanted nothing more in life then to learn how to code.");
         System.out.println("So the frog spent every day and every night working to learn how to code.");
     }
 }
