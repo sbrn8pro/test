fa6bddf78be6490be5368055e83f03b927d87c9b
Lexa13529 <124809038+Lexa13529@users.noreply.github.com>
2023-02-09 11:39:29 -0500

    Update BrigittesTest.java

--- BrigittesTest.java
+++ BrigittesTest.java
@@ -11,6 +11,7 @@ public class BrigittesTest {
         System.out.println("Collaborate below!!");
         System.out.println("Once upon a time there was a frog. This frog wanted nothing more in life then to learn how to code.");
         System.out.println("So the frog spent every day and every night working to learn how to code.");
+        System.out.println("The frog learned to code by studying hard.");
     }
 }
 
